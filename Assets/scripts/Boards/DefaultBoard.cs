91c12530aee289178f04809b75243b4098a436a3
Jorge <Jcvarela@live.com>
2018-04-26 14:20:21 -0400

    working on Board

--- Assets/scripts/Boards/DefaultBoard.cs
+++ Assets/scripts/Boards/DefaultBoard.cs
@@ -5,16 +5,133 @@ using UnityEngine;
 public class DefaultBoard : Board
 {
 
+    //TODO: MUST FOLLOW THIS STANDARD!!!!!!!!!!!!!!!!!!!!!
     //row by col
+    //x -> row 
+    //y -> col
     [SerializeField] private Transform[,] elementsInBoard = new Transform[20,10];
 
+
+    [SerializeField] private GameObject s;
+
     // Use this for initialization
-    void Start () {
-		
-	}
+    void Start ()
+    {
+
+        GameObject obj = Instantiate(s, new Vector3(0,1,0), Quaternion.identity);
+
+
+        GetChildrenFromShape(obj.GetComponent<Shape>());
+
+        GameObject obj2 = Instantiate(s, new Vector3(0, 3, 0), Quaternion.identity);
+        Shape sha = obj2.GetComponent<Shape>();
+
+        Debug.Log("Can IT Move " + CanShapeMoveTo(sha,new Vector3(0,2,0)));
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    public bool GetChildrenFromShape(Shape shape)
+    {
+        Transform[] elements = shape.GetAllChildTransform();
+        if (elements == null || elements.Length == 0) return true;
+
+        foreach (Transform i in elements)
+        {
+            i.parent = null;
+
+            if (IsBoardEmptyAt(i.position))
+            {
+                AddElementAt(i, i.position);
+            }
+
+        }
+
+        Destroy(shape);
+        return true;
+    }
+
+    public bool AddElementAt(Transform ele, Vector3 pos)
+    {
+        int row = GetRow(pos);
+        int col = GetCol(pos);
+
+        if (IsOutOfBound(row, col)) return true;
+
+        elementsInBoard[row, col] = ele;
+
+        return true;
+    }
+
+    public bool CanShapeMoveTo(Shape shape, Vector3 incPos)
+    {
+        Vector3[] positions = shape.GetVectorSpace();
+        if (positions == null || positions.Length == 0) return true;
+
+
+        foreach (Vector3 i in positions)
+        {
+            Vector3 newPos = i + incPos;
+            if (!IsBoardEmptyAt(newPos))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool RemoveElementAt(Vector2 pos)
+    {
+        int row = GetRow(pos);
+        int col = GetCol(pos);
+
+        if (!IsBoardEmptyAt(row, col))
+        {
+            Transform ele = elementsInBoard[row, col];
+            Destroy(ele);
+            elementsInBoard[row, col] = null;
+        }
+
+        return true;
+    }
+
+    public bool IsBoardEmptyAt(Vector2 pos)
+    {
+        int row = GetRow(pos);
+        int col = GetCol(pos);
+        return IsBoardEmptyAt(row, col);
+    }
+
+    public bool IsBoardEmptyAt(int row, int col)
+    {
+        if (IsOutOfBound(row, col)) return false;
+
+        return (elementsInBoard[row, col] == null);
+    }
+
+    public bool IsOutOfBound(int row, int col)
+    {
+
+        if (row < 0 || row >= elementsInBoard.Length)  return true;
+        
+        if (col < 0 || col >= elementsInBoard.GetLength(1))  return true;
+        
+        return false;
+    }
+
+    private int GetRow(Vector3 pos)
+    {
+        return Mathf.RoundToInt(pos.x);
+    }
+
+    private int GetCol(Vector3 pos)
+    {
+        return Mathf.RoundToInt(pos.y);
+    }
 }
--- Assets/scripts/Shapes/Shape.cs
+++ Assets/scripts/Shapes/Shape.cs
@@ -9,41 +9,54 @@ public class Shape : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
-	    bool a = RemoveIfEmpty();
-
-        Debug.Log(a);
-
-        RemoveElementAt(new Vector3(1, 11, 0));
-
-        Vector3[] b = GetVectorSpace();
-
-	  
-
-	    foreach (var i in b)
-	    {
-	        Debug.Log(i);
-	    }
+//	    bool a = RemoveIfEmpty();
+//
+//        Debug.Log(a);
+//
+//        //RemoveElementAt(new Vector3(1, 11, 0));
+//	    RotateShapeBy(new Vector3(0, 0, 90));
+//	    RotateShapeBy(new Vector3(0, 0, 90));
+//        Vector3[] b = GetVectorSpace();
+//
+//	    foreach (var i in b)
+//	    {
+//	        Debug.Log(i);
+//	    }
 
 	}
 
-    public bool RemoveElementAt(Vector3 pos)
+    //    public bool RemoveElementAt(Vector3 pos)
+    //    {
+    //        Transform[] chiTransforms = GetAllChildTransform();
+    //        if (chiTransforms == null || chiTransforms.Length == 0)
+    //        {
+    //            return false;
+    //        }
+    //
+    //        foreach (var i in chiTransforms)
+    //        {
+    //            if (EqualToEachOther(i.position,pos))
+    //            {
+    //                Destroy(i.gameObject);
+    //                return true;
+    //            }
+    //        }
+    //
+    //        return false;
+    //    }
+
+    /// <summary>
+    /// Rotate Shape by givin degree 
+    ///     exp new Vector(0,0,180);
+    /// </summary>
+    /// <param name="eulerVector3"></param>
+    /// <returns>Returns true if it was able to rotate</returns>
+    public bool RotateShapeBy(Vector3 eulerVector3)
     {
-        Transform[] chiTransforms = GetAllObjects();
-        if (chiTransforms == null || chiTransforms.Length == 0)
-        {
-            return false;
-        }
+        eulerVector3 += transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(eulerVector3);
 
-        foreach (var i in chiTransforms)
-        {
-            if (EqualToEachOther(i.position,pos))
-            {
-                Destroy(i.gameObject);
-                return true;
-            }
-        }
-
-        return false;
+        return true;
     }
 
     private bool EqualToEachOther(Vector3 a1, Vector3 a2)
@@ -53,11 +66,15 @@ public class Shape : MonoBehaviour {
         return dif.magnitude < 0.10;
     }
 
+    /// <summary>
+    /// Get an array of Vector3 that represent the spaces the shape occupies
+    /// </summary>
+    /// <returns></returns>
     public Vector3[] GetVectorSpace()
     {
-        Transform[] childrenTransforms = GetAllObjects();
+        Transform[] childrenTransforms = GetAllChildTransform();
         
-        if (childrenTransforms == null || childrenTransforms.Length == 0)
+        if (IsEmpty(childrenTransforms))
         {
             return null;
         }
@@ -73,9 +90,13 @@ public class Shape : MonoBehaviour {
 
     }
 
+    /// <summary>
+    /// Delete shape if it doesn't have any children
+    /// </summary>
+    /// <returns></returns>
     public bool RemoveIfEmpty()
     {
-        Transform[] temp = GetAllObjects();
+        Transform[] temp = GetAllChildTransform();
 
         if (temp == null || temp.Length == 0)
         {
@@ -86,9 +107,18 @@ public class Shape : MonoBehaviour {
         return false;
     }
 
-    private Transform[] GetAllObjects()
+    /// <summary>
+    /// Get all the child for this shape GameObject 
+    /// </summary>
+    /// <returns></returns>
+    public Transform[] GetAllChildTransform()
     {
         Transform[] temp = GetComponentsInChildren<Transform>();
+        if (IsEmpty(temp))
+        {
+            return null;
+        }
+
         Transform[] output = new Transform[temp.Length - 1];
 
         int inc = 0;
@@ -103,4 +133,13 @@ public class Shape : MonoBehaviour {
         return output;
     }
 
+    private bool IsEmpty(System.Object[] objA)
+    {
+        if (objA == null || objA.Length == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
